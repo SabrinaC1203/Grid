bb57d68865e9b21a24f1e645a8d0b0b533a1bdc5
SabrinaC1203 <sab.campisi@gmail.com>
2025-09-01 13:23:41 +0200

    Separate HTML and java files

---
+++ java.js
@@ -0,0 +1,42 @@
+    const grid = document.querySelector("div");
+    const button = document.querySelector("button");
+
+    function createSquares (x) {
+        for (let i = 0; i < x * x; i++){
+            const square = document.createElement("div");
+            square.classList.add("cell");
+            const size = 100 / x;
+            square.style.width = size + "%";
+            square.style.height = size + "%";
+            hoverSquares(square);
+            grid.appendChild(square);
+        } 
+    } (createSquares(16));
+
+    function hoverSquares (square){
+        square.addEventListener(
+        "mouseover", (e) => {
+        e.target.style.backgroundColor = "gray";
+    });
+        square.addEventListener(
+        "mouseout", (e) => {
+        e.target.style.backgroundColor = "";
+    });
+    }
+
+    function getInput () {
+        const input = prompt("Give a number between 1 and 100 to create a new grid");
+        if(!input || isNaN(Number(input)) || input > 100 || input < 1) { 
+            return null; 
+        } return Number(input);
+    }
+    
+    
+    function changeGrid () {
+        const size = getInput();
+        if (!size) return;
+
+        grid.innerHTML = "";
+        createSquares(size);
+    }
+    button.addEventListener("click", changeGrid);
